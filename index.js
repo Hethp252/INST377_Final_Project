6c5e4a583ea62a00d5b2861003b07f64fef8b3a3
hethp252 <hethp252@gmail.com>
2026-05-16 17:14:28 -0400

    Testing

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(express.json());
 
 // --- HARDCODED KEYS ---
-const SUPABASE_URL = 'PASTE_YOUR_SUPABASE_URL_HERE';
-const SUPABASE_KEY = 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE';
-const FINNHUB_API_KEY = 'PASTE_YOUR_FINNHUB_KEY_HERE';
+const SUPABASE_URL = 'https://fksiydmwbyxxhzaihfqn.supabase.co/rest/v1/';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrc2l5ZG13Ynl4eGh6YWloZnFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MzQ1NjUsImV4cCI6MjA5NDMxMDU2NX0._GpovLiI9IxuCzaTx8quG3pewx_T4tzN9JVk17nXc6M';
+const FINNHUB_API_KEY = 'd84dqvpr01qutij8p140d84dqvpr01qutij8p14g';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
